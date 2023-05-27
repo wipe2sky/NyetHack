e0096c98cc10bc5f9a3f74bff2b66c11c97309ae
Sergey Kurtsevich (Ext) <sergey.kurtsevich@tietoevry.com>
2023-05-27 20:54:01 +0300

    added a world map and the ability to move

--- src/main/kotlin/com/kurtsevich/nyethack/Game.kt
+++ src/main/kotlin/com/kurtsevich/nyethack/Game.kt
@@ -1,7 +1,6 @@
 package com.kurtsevich.nyethack
 
 fun main(args: Array<String>) {
-
     Game.play()
 }
 
@@ -9,10 +8,14 @@ fun main(args: Array<String>) {
 object Game {
 
     private val player = Player("Madrigal")
-    private val currentRoom: Room = TownSquare()
+    private var currentRoom: Room = TownSquare()
+    private val worldMap = listOf(
+        listOf(currentRoom, Room("Tavern"), Room("Back Room")),
+        listOf(Room("Long Corridor"), Room("Generic Room"))
+    )
 
     init {
-        println("Welcome, adventurer.")
+        println("Welcome, adventurer.\n")
         player.castFireball()
     }
 
@@ -28,6 +31,25 @@ object Game {
         }
     }
 
+    private fun move(directionInput: String) =
+        try {
+            val direction = Direction.valueOf(directionInput.uppercase())
+            val newPosition = direction.updateCoordinate(player.currentPosition)
+
+            if (!newPosition.isInBounds) {
+                throw IllegalStateException("$direction is out of bounds")
+            }
+
+            val newRoom = worldMap[newPosition.y][newPosition.x]
+            player.currentPosition = newPosition
+            currentRoom = newRoom
+
+            "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+        } catch (e: Exception) {
+            "Invalid direction: $directionInput"
+        }
+
+
     private fun printPlayerStatus(player: Player) {
         println("(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})")
         println("${player.name} ${player.formatHealthStatus()}")
@@ -40,7 +62,8 @@ object Game {
 
         private fun commandNotFound() = "I'm not quite sure what you're trying to do!\n"
 
-        fun processCommand() = when(command.lowercase()){
+        fun processCommand() = when (command.lowercase()) {
+            "move" -> move(argument)
             else -> commandNotFound()
         }
     }
--- src/main/kotlin/com/kurtsevich/nyethack/Room.kt
+++ src/main/kotlin/com/kurtsevich/nyethack/Room.kt
@@ -3,7 +3,7 @@ package com.kurtsevich.nyethack
 open class Room(val name: String) {
 
     protected open val dangerLevel = 5
-    fun description() = "Room: $name\n " +
+    fun description() = "Room: $name\n" +
             "Danger level: $dangerLevel"
 
     open fun load() = "Nothing much to see here..."
